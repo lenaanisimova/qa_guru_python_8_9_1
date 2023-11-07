42f83a925dde67dc4ea3065c8fb47b61b2e63225
elenaanisimova <elena.anisimova@lenvendo.ru>
2023-11-07 18:53:57 +0100

    lesson 9

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,28 +1,9 @@
+#
 import allure
 from selene.support import by
 from selene.support.conditions import be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
-
-
-def test_dynamic_steps():
-    with allure.step("Открываем главную страницу"):
-        browser.open("https://github.com")
-
-    with allure.step("Ищем репозитория"):
-        s(".header-search-button").click()
-        s("#query-builder-test").send_keys("eroshenkoam/allure-example")
-        s("#query-builder-test").press_enter()
-
-    with allure.step("Переходим по ссылке репозитория"):
-        s(by.link_text("eroshenkoam/allure-example")).click()
-
-    with allure.step("Открываем таб Issues"):
-        s("#issues-tab").click()
-
-    with allure.step("Проверяем наличие Issue с номером 76"):
-        s(by.partial_text("#76")).should(be.visible)
-
 
 def test_decorator_steps():
     open_main_page()
@@ -39,9 +20,9 @@ def open_main_page():
 
 @allure.step("Ищем репозитория {repo}")
 def search_for_repository(repo):
-    s(".header-search-input").click()
-    s(".header-search-input").send_keys(repo)
-    s(".header-search-input").submit()
+    s(".header-search-button").click()
+    s("#query-builder-test").send_keys(repo)
+    s("#query-builder-test").press_enter()
 
 
 @allure.step("Переходим по ссылке репозитория {repo}")
